eb9bd08db3c775702d2e1fff99e73621cea73b30
Justinholb24 <102241021+Justinholb24@users.noreply.github.com>
2022-08-02 02:16:58 -0500

    Added features
    Add rainbow and reset features

--- script.js
+++ script.js
@@ -1,22 +1,73 @@
-function makeBlocks() {
-    for (var i = 0; i < 16; i++) {
+const gridSize = 20;
+
+function changeSize() {
+    const gridSize = prompt("How many rows/columns would you like?");  
+    removeAllGridCells()  
+    makeBlocks(gridSize);
+    
+}
+
+
+function makeBlocks(gridSize) {
+    for (var i = 0; i < gridSize; i++) {
         var row = document.createElement('div');
         row.className = "row";
-        for (var j = 0; j < 16; j++) {
+        for (var j = 0; j < gridSize; j++) {
             var box = document.createElement('div');
             box.className = "box";
             row.appendChild(box);
         }                
         document.getElementById('container').appendChild(row);
     }
+
+    
 }
 
-makeBlocks();
+makeBlocks(gridSize);
 
 const boxes = document.querySelectorAll('.box')
+
 boxes.forEach(box => {
     box.addEventListener('mouseover', function handleClick(event) {
   
       box.setAttribute('style', 'background-color: black;');
     });
   });
+
+  
+  function reset() {
+    boxes.forEach(box => {
+        box.setAttribute('style', 'background-color: white;')
+    })
+  }
+
+
+  function removeAllGridCells() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+function rainbowMode() {
+    boxes.forEach(box => box.addEventListener('mouseover', () => { 
+            let R = Math.floor(Math.random() * 256);
+            let G = Math.floor(Math.random() * 256);
+            let B = Math.floor(Math.random() * 256);
+            const RGB = `rgb(${R},${G},${B})`;
+            box.style.background = RGB;
+    }))
+}
+
+
+function blackMode() {
+    const boxes = document.querySelectorAll('.box')
+
+boxes.forEach(box => {
+    box.addEventListener('mouseover', function handleClick(event) {
+  
+      box.setAttribute('style', 'background-color: black;');
+    });
+  });
+}
+
+
